17804e41698d31d25ec7567eaeaa464f1fad499b
claudioharu <claudioharu@gmail.com>
2015-05-10 19:58:12 -0300

    obstacles

--- MyTronBot.py
+++ MyTronBot.py
@@ -23,24 +23,35 @@ def flood_fill(board, startpos):
 
 	return sorted(done)
 
-def Frame():
+def Frame(board):
 	mapFrame = []
 	# print sorted(mapFrame)
-	for i in range (0,15):
+	for i in range (0,board.width):
 		mapFrame.append([0,i])
 
-	for i in range (0, 15):
-		mapFrame.append([15-1, i])
+	for i in range (0, board.width):
+		mapFrame.append([board.height-1, i])
 
-	for i in range (1, 15-1):
+	for i in range (1, board.height-1):
 		mapFrame.append([i,0])
 
-	for i in range (1, 15-1):
-		mapFrame.append([i,15-1])
+	for i in range (1, board.height-1):
+		mapFrame.append([i,board.width-1])
 
 	return mapFrame
 
-def which_move(board, path):
+def Obstacles(board):
+	obs = []
+	for x in range(0, board.width):
+		for y in range (0, board.height):
+			point = x, y
+			if not board.passable(point):
+				if board.me() != point and board.them() != point:
+					obs.append([x, y])
+	return obs
+
+
+def which_move(board):
 	# check walls
     # value = flood_fill(board,  board.me())
     # print value
@@ -50,36 +61,46 @@ def which_move(board, path):
 
 	# print sorted(mapFrame)
 
-	
-	# if path is None:
-	#     return random.choice(board.moves())
-	# else:
-	# print "Path found:", path
-	value = path[1].x, path[1].y
-	path.pop(0)
-	
-	if (value[0] - board.me()[0]) == 0 and (value[1] - board.me()[1]) == 1: 
-		return tron.EAST
-	if (value[0] - board.me()[0]) == 0 and (value[1] - board.me()[1]) == -1:
-		return tron.WEST
-	if (value[0] - board.me()[0]) == 1 and (value[1] - board.me()[1]) == 0:
-		return tron.SOUTH
-	if (value[0] - board.me()[0]) == -1 and (value[1] - board.me()[1]) == 0:
-		return tron.NORTH
-	# if (value[0] - board.me()[0]) == 0 and (value[1] - board.me()[1]) == 0:
-	# 	return random.choice(board.moves())
+
+	graph, nodes = make_graph({"width": board.width, "height": board.height, "obstacle": Frame(board)})
+	# print graph
+	paths = None
+	paths = AStarGrid(graph)
+	start, end = nodes[board.me()[0]][board.me()[1]], nodes[board.them()[0]][board.them()[1]]
+	path = paths.search(start, end)
+	# print path
+	# you do not need to modify this part
+
+	if path is None:
+		return random.choice(board.moves())
+	else:
+		# print "Path found:", path
+		value = path[1].x, path[1].y
+		path.pop(0)
+		
+		if (value[0] - board.me()[0]) == 0 and (value[1] - board.me()[1]) == 1:
+			
+			if not board.passable(value):
+				return random.choice(board.moves())
+
+			return tron.EAST
+		if (value[0] - board.me()[0]) == 0 and (value[1] - board.me()[1]) == -1:
+			if not board.passable(value):
+				return random.choice(board.moves())
+			return tron.WEST
+		if (value[0] - board.me()[0]) == 1 and (value[1] - board.me()[1]) == 0:
+			if not board.passable(value):
+				return random.choice(board.moves())
+			return tron.SOUTH
+		if (value[0] - board.me()[0]) == -1 and (value[1] - board.me()[1]) == 0:
+			if not board.passable(value):
+				return random.choice(board.moves())
+			return tron.NORTH
+		# if (value[0] - board.me()[0]) == 0 and (value[1] - board.me()[1]) == 0:
+		# 	return random.choice(board.moves())
 
 
-graph, nodes = make_graph({"width": 15, "height": 15, "obstacle": Frame()})
-# print graph
-paths = None
-paths = AStarGrid(graph)
-start, end = nodes[1][1], nodes[13][13]
-path = paths.search(start, end)
-# print path
-# you do not need to modify this part
+
 
 for board in tron.Board.generate():
-    tron.move(which_move(board, path))
-
-
+    tron.move(which_move(board))

0d64c953c90d03ae99c39a359fbd78635304853f
claudioharu <claudioharu@gmail.com>
2015-05-12 18:57:02 -0300

    usando flood para escapar de possíveis colisões

--- MyTronBot.py
+++ MyTronBot.py
@@ -23,22 +23,19 @@ def flood_fill(board, startpos):
 
 	return sorted(done)
 
-def Frame(board):
-	mapFrame = []
-	# print sorted(mapFrame)
-	for i in range (0,board.width):
-		mapFrame.append([0,i])
+def new_flood_fill(board, startpos, obst):
+	expand = [startpos]
+	done = []
+	while len(expand) is not 0:
+		pos = expand.pop()
+		done.append(pos)
+		for dir in tron.DIRECTIONS:
+			dest = board.rel(dir, pos)
+			if board.passable(dest) and dest != obst and dest not in done and dest not in expand:
+				expand.append(dest)
 
-	for i in range (0, board.width):
-		mapFrame.append([board.height-1, i])
+	return sorted(done)
 
-	for i in range (1, board.height-1):
-		mapFrame.append([i,0])
-
-	for i in range (1, board.height-1):
-		mapFrame.append([i,board.width-1])
-
-	return mapFrame
 
 def Obstacles(board):
 	obs = []
@@ -50,13 +47,31 @@ def Obstacles(board):
 					obs.append([x, y])
 	return obs
 
+def wallBot(board):
+	# wall bot
+		ORDER = list(tron.DIRECTIONS)
+		random.shuffle(ORDER)
+		decision = board.moves()[0]
+		for dir in ORDER:
+
+			# where we will end up if we move this way
+			dest = board.rel(dir)
+
+			# destination is passable?
+			if not board.passable(dest):
+				continue
+
+			# positions adjacent to the destination
+			adj = board.adjacent(dest)
+
+			# if any wall adjacent to the destination
+			if any(board[pos] == tron.WALL for pos in adj):
+				decision = dir
+				break
+
+		return decision
+
 def which_move(board):
-	# check walls
-    # value = flood_fill(board,  board.me())
-    # print value
-
-	# print sorted(mapFrame)
-
 
 	graph, nodes = make_graph({"width": board.width, "height": board.height, "obstacle": Obstacles(board)})
 	# print graph
@@ -65,10 +80,10 @@ def which_move(board):
 	start, end = nodes[board.me()[0]][board.me()[1]], nodes[board.them()[0]][board.them()[1]]
 	path = paths.search(start, end)
 	# print path
-	# you do not need to modify this part
 
 	if path is None:
-		return random.choice(board.moves())
+		return wallBot(board)
+
 	else:
 		# print "Path found:", path
 		value = path[1].x, path[1].y
@@ -76,23 +91,90 @@ def which_move(board):
 		
 		if (value[0] - board.me()[0]) == 0 and (value[1] - board.me()[1]) == 1:
 			if not board.passable(value):
-				pos = flood_fill(board, board.me())
-				print pos
 				return random.choice(board.moves())
 			else:
-				return tron.EAST
+				dest = board.rel(tron.WEST, board.them())
+				if dest == value:
+					room1 = len(new_flood_fill(board, board.rel(tron.NORTH, board.me()), board.rel(tron.EAST, board.me())))
+					room2 = len(new_flood_fill(board, board.rel(tron.SOUTH, board.me()), board.rel(tron.EAST, board.me())))
+					
+					if room1 > room2:
+						if board.passable( board.rel(tron.NORTH, board.me())):
+							return tron.NORTH
+						else: return random.choice(board.moves())
+
+					else : 
+						if board.passable(board.rel(tron.SOUTH, board.me())):
+							return tron.SOUTH 
+						else: return random.choice(board.moves())
+
+					
+				else:
+					return tron.EAST
+
 		if (value[0] - board.me()[0]) == 0 and (value[1] - board.me()[1]) == -1:
 			if not board.passable(value):
 				return random.choice(board.moves())
-			return tron.WEST
+			else:
+				dest = board.rel(tron.EAST, board.them())
+				if dest == value:
+					room1 = len(new_flood_fill(board, board.rel(tron.NORTH, board.me()), board.rel(tron.EAST, board.me())))
+					room2 = len(new_flood_fill(board, board.rel(tron.SOUTH, board.me()), board.rel(tron.EAST, board.me())))
+					
+					if room1 > room2:
+						if board.passable( board.rel(tron.NORTH, board.me())):
+							return tron.NORTH
+						else: return random.choice(board.moves())
+
+					else : 
+						if board.passable(board.rel(tron.SOUTH, board.me())):
+							return tron.SOUTH 
+						else: return random.choice(board.moves())
+				else:
+					return tron.WEST
+	
 		if (value[0] - board.me()[0]) == 1 and (value[1] - board.me()[1]) == 0:
 			if not board.passable(value):
 				return random.choice(board.moves())
-			return tron.SOUTH
+			else:
+				dest = board.rel(tron.NORTH, board.them())
+				if dest == value:
+					room1 = len(new_flood_fill(board, board.rel(tron.WEST, board.me()), board.rel(tron.EAST, board.me())))
+					room2 = len(new_flood_fill(board, board.rel(tron.EAST, board.me()), board.rel(tron.EAST, board.me())))
+					
+					if room1 > room2:
+						if board.passable( board.rel(tron.WEST, board.me())):
+							return tron.WEST
+						else: return random.choice(board.moves())
+
+					else : 
+						if board.passable(board.rel(tron.EAST, board.me())):
+							return tron.EAST 
+						else: return random.choice(board.moves())
+
+				else :
+					return tron.SOUTH
+	
 		if (value[0] - board.me()[0]) == -1 and (value[1] - board.me()[1]) == 0:
 			if not board.passable(value):
 				return random.choice(board.moves())
-			return tron.NORTH
+			else:
+				dest = board.rel(tron.SOUTH, board.them())
+				if dest == value:
+					room1 = len(new_flood_fill(board, board.rel(tron.WEST, board.me()), board.rel(tron.EAST, board.me())))
+					room2 = len(new_flood_fill(board, board.rel(tron.EAST, board.me()), board.rel(tron.EAST, board.me())))
+					
+					if room1 > room2:
+						if board.passable( board.rel(tron.WEST, board.me())):
+							return tron.WEST
+						else: return random.choice(board.moves())
+
+					else : 
+						if board.passable(board.rel(tron.EAST, board.me())):
+							return tron.EAST 
+						else: return random.choice(board.moves())
+				else:
+					return tron.NORTH
 		# if (value[0] - board.me()[0]) == 0 and (value[1] - board.me()[1]) == 0:
 		# 	return random.choice(board.moves())
 
